900279ff49ee8b98045fb343a2cd7a304beafb00
rosa lee <rosalee0116@gmail.com>
2019-07-26 00:22:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -4,17 +4,10 @@ def hopper
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+			}
+        programmer_hash[:grace_hopper]
 
 
 end
