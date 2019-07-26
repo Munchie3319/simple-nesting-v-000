0a12d68c6f115278092cd4b913c74c78e44c747c
rosa lee <rosalee0116@gmail.com>
2019-07-26 00:26:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -30,17 +30,11 @@ def dennis_ritchies_language
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
           :languages => ["C"]
+
         }
      }
+		 programmer_hash[:dennis_ritchie][languages][0]
 end
 
 def adding_matz
